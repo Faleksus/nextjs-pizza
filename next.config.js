31342aa43277c36894820585f10082cc934765a8
Oleksiy Frolov <avralgroup@gmail.com>
2023-03-14 15:44:44 +0200

    corrected

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   eslint: {
     dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
   },
+  distDir: 'build',
 }
 
 module.exports = nextConfig
