12a9ec56addcde6dd051f8f8151edda8d69c690b
Faleksus <70090594+Faleksus@users.noreply.github.com>
2023-03-14 13:44:49 +0000

    Deploying to gh-pages from @ Faleksus/nextjs-pizza@31342aa43277c36894820585f10082cc934765a8 🚀

---
+++ server/pages/_app.js
@@ -0,0 +1,411 @@
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 165:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"container": "Footer_container__bwQe4",
+	"item": "Footer_item__OoOG2",
+	"card": "Footer_card__nIgtz",
+	"title": "Footer_title__LdEX5",
+	"text": "Footer_text__TPgcV",
+	"motto": "Footer_motto__ZdSEd"
+};
+
+
+/***/ }),
+
+/***/ 565:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"container": "Navbar_container__ql08n",
+	"item": "Navbar_item__4Aj__",
+	"callButton": "Navbar_callButton__1NZQg",
+	"texts": "Navbar_texts__DNT2d",
+	"text": "Navbar_text__fp35b",
+	"list": "Navbar_list__j1FKl",
+	"listItem": "Navbar_listItem__4BM2Z",
+	"cart": "Navbar_cart__VXkyy",
+	"counter": "Navbar_counter__5fFKF"
+};
+
+
+/***/ }),
+
+/***/ 882:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ App)
+});
+
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+// EXTERNAL MODULE: ./src/styles/globals.css
+var globals = __webpack_require__(108);
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__(689);
+// EXTERNAL MODULE: ./node_modules/next/image.js
+var next_image = __webpack_require__(675);
+var image_default = /*#__PURE__*/__webpack_require__.n(next_image);
+// EXTERNAL MODULE: ./src/components/Footer/Footer.module.css
+var Footer_module = __webpack_require__(165);
+var Footer_module_default = /*#__PURE__*/__webpack_require__.n(Footer_module);
+;// CONCATENATED MODULE: ./src/components/Footer/Footer.jsx
+
+
+
+
+const Footer = ()=>{
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: (Footer_module_default()).container,
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (Footer_module_default()).item,
+                children: /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                    src: "/img/bg.png",
+                    objectFit: "cover",
+                    layout: "fill",
+                    alt: ""
+                })
+            }),
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: (Footer_module_default()).item,
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                        className: (Footer_module_default()).card,
+                        children: /*#__PURE__*/ jsx_runtime_.jsx("h2", {
+                            className: (Footer_module_default()).motto,
+                            children: "OH YES, WE DID.THE LAMA PIZZA, WELL BAKED SLICE OF PIZZA."
+                        })
+                    }),
+                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                        className: (Footer_module_default()).card,
+                        children: [
+                            /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+                                className: (Footer_module_default()).title,
+                                children: "FIND OUR RESTAURANTS"
+                            }),
+                            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("p", {
+                                className: (Footer_module_default()).text,
+                                children: [
+                                    "1654 R. Don Road #304.",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " NewYork, 85022",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " (602) 867-1010"
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("p", {
+                                className: (Footer_module_default()).text,
+                                children: [
+                                    "2356 K. Laquie Rd #235.",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " NewYork, 85022",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " (602) 867-1011"
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("p", {
+                                className: (Footer_module_default()).text,
+                                children: [
+                                    "1614 E. Erwin St #104.",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " NewYork, 85022",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " (602) 867-1012"
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("p", {
+                                className: (Footer_module_default()).text,
+                                children: [
+                                    "1614 W. Caroll St #125.",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " NewYork, 85022",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " (602) 867-1013"
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                        className: (Footer_module_default()).card,
+                        children: [
+                            /*#__PURE__*/ jsx_runtime_.jsx("h1", {
+                                className: (Footer_module_default()).title,
+                                children: "WORKING HOURS"
+                            }),
+                            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("p", {
+                                className: (Footer_module_default()).text,
+                                children: [
+                                    "MONDAY UNTIL FRIDAY",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " 9:00 – 22:00"
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("p", {
+                                className: (Footer_module_default()).text,
+                                children: [
+                                    "SATURDAY - SUNDAY",
+                                    /*#__PURE__*/ jsx_runtime_.jsx("br", {}),
+                                    " 12:00 – 24:00"
+                                ]
+                            })
+                        ]
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* harmony default export */ const Footer_Footer = (Footer);
+
+// EXTERNAL MODULE: ./src/components/Navbar/Navbar.module.css
+var Navbar_module = __webpack_require__(565);
+var Navbar_module_default = /*#__PURE__*/__webpack_require__.n(Navbar_module);
+;// CONCATENATED MODULE: ./src/components/Navbar/Navbar.jsx
+
+
+
+
+const Navbar = ()=>{
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+        className: (Navbar_module_default()).container,
+        children: [
+            /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                className: (Navbar_module_default()).item,
+                children: [
+                    /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                        className: (Navbar_module_default()).callButton,
+                        children: /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                            src: "/img/telephone.png",
+                            alt: "",
+                            width: 20,
+                            height: 20
+                        })
+                    }),
+                    /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                        className: (Navbar_module_default()).texts,
+                        children: [
+                            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                                className: (Navbar_module_default()).text,
+                                children: "ORDER NOW"
+                            }),
+                            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                                className: (Navbar_module_default()).text,
+                                children: "096 514 83 83"
+                            })
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (Navbar_module_default()).item,
+                children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("ul", {
+                    className: (Navbar_module_default()).list,
+                    children: [
+                        /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                            className: (Navbar_module_default()).listItem,
+                            children: "Homepage"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                            className: (Navbar_module_default()).listItem,
+                            children: "Products"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                            className: (Navbar_module_default()).listItem,
+                            children: "Menu"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                            src: "/img/logo.png",
+                            alt: "logo",
+                            width: 160,
+                            height: 69
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                            className: (Navbar_module_default()).listItem,
+                            children: "Events"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                            className: (Navbar_module_default()).listItem,
+                            children: "Blog"
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("li", {
+                            className: (Navbar_module_default()).listItem,
+                            children: "Contact"
+                        })
+                    ]
+                })
+            }),
+            /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                className: (Navbar_module_default()).item,
+                children: /*#__PURE__*/ (0,jsx_runtime_.jsxs)("div", {
+                    className: (Navbar_module_default()).cart,
+                    children: [
+                        /*#__PURE__*/ jsx_runtime_.jsx((image_default()), {
+                            src: "/img/cart.png",
+                            alt: "",
+                            width: 30,
+                            height: 30
+                        }),
+                        /*#__PURE__*/ jsx_runtime_.jsx("div", {
+                            className: (Navbar_module_default()).counter,
+                            children: "2"
+                        })
+                    ]
+                })
+            })
+        ]
+    });
+};
+/* harmony default export */ const Navbar_Navbar = (Navbar);
+
+;// CONCATENATED MODULE: ./src/components/Layouts/Layout.jsx
+
+
+
+
+const Layout = ({ children  })=>{
+    return /*#__PURE__*/ (0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+        children: [
+            /*#__PURE__*/ jsx_runtime_.jsx(Navbar_Navbar, {}),
+            children,
+            /*#__PURE__*/ jsx_runtime_.jsx(Footer_Footer, {})
+        ]
+    });
+};
+/* harmony default export */ const Layouts_Layout = (Layout);
+
+;// CONCATENATED MODULE: ./src/pages/_app.js
+
+
+
+function App({ Component , pageProps  }) {
+    return /*#__PURE__*/ jsx_runtime_.jsx(Layouts_Layout, {
+        children: /*#__PURE__*/ jsx_runtime_.jsx(Component, {
+            ...pageProps
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 108:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 918:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/amp-context.js");
+
+/***/ }),
+
+/***/ 732:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/amp-mode.js");
+
+/***/ }),
+
+/***/ 796:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/head-manager-context.js");
+
+/***/ }),
+
+/***/ 486:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-blur-svg.js");
+
+/***/ }),
+
+/***/ 744:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config-context.js");
+
+/***/ }),
+
+/***/ 843:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-config.js");
+
+/***/ }),
+
+/***/ 552:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/image-loader");
+
+/***/ }),
+
+/***/ 470:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/side-effect.js");
+
+/***/ }),
+
+/***/ 618:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("next/dist/shared/lib/utils/warn-once.js");
+
+/***/ }),
+
+/***/ 689:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = __webpack_require__.X(0, [636,675], () => (__webpack_exec__(882)));
+module.exports = __webpack_exports__;
+
+})();
